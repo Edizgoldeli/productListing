46172d3e90d4ceb95396ab670272643db257f551
Ediz <edizgoldeli@hotmail.com>
2023-04-27 18:59:49 +0300

    Product Listing Application

---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,28 @@
+import { Task } from './Task';
+
+export const TASKS: Task[] = [
+  {
+    id: 1,
+    text: 'Doctors Appointment',
+    prdctDetails: 'May 5th at 2:30pm',
+   
+  },
+  {
+    id: 2,
+    text: 'Meeting at School',
+    prdctDetails: 'May 6th at 1:30pm',
+    
+  },
+  {
+    id: 3,
+    text: 'Food Shopping',
+    prdctDetails: 'May 7th at 12:30pm',
+    
+  },
+  {
+    id: 4,
+    text: 'Food1 Shopping',
+    prdctDetails: 'May 7th at 12:30pm',
+   
+  }
+];
